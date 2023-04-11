87ed2cc7afa96234a8bcd4a79832e2d705eccf9d
Moe Zaky <devmuzaky@gmail.com>
2023-04-11 13:29:31 +0200

    creating loading component

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,20 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'loading',
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css']
+  selector: 'loading', templateUrl: './loading.component.html', styleUrls: ['./loading.component.css']
 })
 export class LoadingComponent implements OnInit {
 
-
   constructor() {
-
   }
 
   ngOnInit() {
 
   }
-
-
 }
