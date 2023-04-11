a3ea23acd215d4f9773a3c679a19a01a1ef01e73
Moe Zaky <devmuzaky@gmail.com>
2023-04-11 13:31:43 +0200

    creating search component

---
+++ src/app/search-lessons/search-lessons.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'course', templateUrl: './search-lessons.component.html', styleUrls: ['./search-lessons.component.css']
+})
+export class SearchLessonsComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+}
