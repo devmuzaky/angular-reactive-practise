324444616c4d78a100365a204e180d3c02b21784
Moe Zaky <devmuzaky@gmail.com>
2023-04-11 13:28:05 +0200

    creating lesson component

---
+++ src/app/lesson/lesson.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'lesson',
+  templateUrl: './lesson.component.html',
+  styleUrls: ['./lesson.component.css']
+})
+export class LessonComponent {
+
+
+}
