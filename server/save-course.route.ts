7890ab6fa3ca41c811d4ca96f7f3b506b2ac1142
Moe Zaky <devmuzaky@gmail.com>
2023-04-12 00:31:03 +0200

    Handling errors at edit course dialog by the messages service

--- server/save-course.route.ts
+++ server/save-course.route.ts
@@ -5,17 +5,14 @@ import {setTimeout} from 'timers';
 
 export function saveCourse(req: Request, res: Response) {
 
-  /*
-  console.log("ERROR saving course!");
+
   res.sendStatus(500);
   return;
 
-  */
+
 
   const id = req.params["id"],
     changes = req.body;
-
-  console.log("Saving course changes", id, JSON.stringify(changes));
 
   const newCourse = {
     ...COURSES[id],
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -5,13 +5,17 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import * as moment from 'moment';
 import {CoursesService} from "../services/courses.service";
 import {LoadingService} from "../loading/loading.service";
+import {MessagesService} from "../messages/messages.service";
+import {catchError} from "rxjs/operators";
+import {throwError} from "rxjs";
 
 @Component({
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.css'],
   providers: [
-    LoadingService
+    LoadingService,
+    MessagesService
   ]
 })
 export class CourseDialogComponent implements AfterViewInit {
@@ -25,7 +29,8 @@ export class CourseDialogComponent implements AfterViewInit {
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) course: Course,
     private coursesService: CoursesService,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
   ) {
 
     this.course = course;
@@ -42,10 +47,17 @@ export class CourseDialogComponent implements AfterViewInit {
   ngAfterViewInit() {
 
   }
+
   save() {
     const changes = this.form.value;
 
-    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
+    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes).pipe(
+      catchError(
+        err => {
+          const message = 'Could not save course';
+          this.messagesService.showMessage(message);
+          return throwError(err)
+        }));
 
     this.loadingService.showLoaderUntilCompleted(saveCourse$)
       .subscribe(
@@ -56,5 +68,4 @@ export class CourseDialogComponent implements AfterViewInit {
   close() {
     this.dialogRef.close();
   }
-
 }
