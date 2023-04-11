cad82534738247caaa312bbd6f85169df390e17d
Moe Zaky <devmuzaky@gmail.com>
2023-04-11 13:30:08 +0200

    creating messages component

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+
+  }
+
+
+  onClose() {
+
+
+  }
+
+}
