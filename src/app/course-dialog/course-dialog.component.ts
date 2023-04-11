685155e3f2b24f0327a75d94ab81ea41a5576337
Moe Zaky <devmuzaky@gmail.com>
2023-04-11 13:25:02 +0200

    creating course dialog component

---
+++ src/app/course-dialog/course-dialog.component.ts
@@ -0,0 +1,48 @@
+import {AfterViewInit, Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {Course} from "../model/course";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import * as moment from 'moment';
+
+@Component({
+  selector: 'course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css']
+})
+export class CourseDialogComponent implements AfterViewInit {
+
+  form: FormGroup;
+
+  course: Course;
+
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course) {
+
+    this.course = course;
+
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required]
+    });
+
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  save() {
+
+    const changes = this.form.value;
+
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+}
