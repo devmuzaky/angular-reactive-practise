f13ca7a780e08a55dbc4479902d280a264383baf
Moe Zaky <devmuzaky@gmail.com>
2023-04-14 16:43:29 +0200

    implementing single data Observable Pattern

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -2,18 +2,24 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
 import {Lesson} from '../model/lesson';
-import {Observable} from "rxjs";
+import {combineLatest, Observable} from "rxjs";
 import {CoursesService} from "../services/courses.service";
+import {map, startWith} from "rxjs/operators";
 
+
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: 'course', templateUrl: './course.component.html', styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
 
-  course$: Observable<Course>;
 
-  lessons$: Observable<Lesson[]>;
+  data$: Observable<CourseData>;
+
 
   constructor(private route: ActivatedRoute, private coursesService: CoursesService) {
 
@@ -22,9 +28,19 @@ export class CourseComponent implements OnInit {
 
   ngOnInit() {
     const courseId = parseInt(this.route.snapshot.paramMap.get('courseId'));
-    this.course$ = this.coursesService.loadCourseById(courseId);
-
-    this.lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+    const course$ = this.coursesService.loadCourseById(courseId).pipe(
+      startWith(null));
+    const lessons$ = this.coursesService.loadAllCourseLessons(courseId).pipe(
+      startWith([])
+    );
+    this.data$ = combineLatest([course$, lessons$]).pipe(
+      map(([course, lessons]) => {
+        return {
+          course,
+          lessons
+        };
+      }
+    ));
   }
 
 
